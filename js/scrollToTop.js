4736fb427587f7272a17d6ea7103f9678d1b25d9
DESKTOP-4EUCJF6\Robert <robertjanjaskowski@gmail.com>
2018-09-16 15:20:08 +0200

    new portfolio

---
+++ js/scrollToTop.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("header").classList.add("header-shadow");
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("header").classList.remove("header-shadow");
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
